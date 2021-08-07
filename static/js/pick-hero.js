c96048e4538b18b9c9b01abf908d1fc699765601
erwintobing15 <erwintobing15@gmail.com>
2021-08-07 10:13:41 +0700

    Updated picking hero for radiant and dire team some additional updated in hero picking logic include : - created 2 new button to pick an active team to pick a hero, with default team to pick first is radiant team - created clear button to refresh the page note : this will work for now, later will update interactive and friendly UI/UX hero picking functionality

--- static/js/pick-hero.js
+++ static/js/pick-hero.js
@@ -1,25 +1,59 @@
-// initialize hero draft counter
-var radiantCounter = 0;
-var direCounter = 0;
+// initialize hero draft counter and picked slot
+var radiantCounter = 1;
+var direCounter = 1;
+
+var teamPicking = "radiant";
 
 // pick a hero, display the hero on team draft,
 // and make hero picked unavailable to pick again
 function pickHero(img) {
-  radiantCounter += 1;
 
   // check if radiant draft not pick 5 hero yet
-  if (radiantCounter <= 5) {
-    console.log("Picked " + radiantCounter + " hero for radiant team.");
-    // display picked hero
-    document.getElementById("radiantHero"+radiantCounter).src = img.src;
-    // assign picked hero as input value
-    document.getElementById("inputRadiant"+radiantCounter).value = img.alt;
+  if (radiantCounter <= 5 || direCounter <= 5) {
+
+    // check if radiant time to pick then pick for radiant
+    // if dire time to pick then pick for dire team
+    // default pick for radiant team
+    if (teamPicking == "radiant")
+    {
+      console.log("Picked " + radiantCounter + " hero for radiant team.");
+      console.log("Radiant slot available : " + (5-radiantCounter));
+
+      // display picked hero
+      document.getElementById("radiantHero"+radiantCounter).src = img.src;
+      document.getElementById("radiantHero"+radiantCounter).alt = img.alt;
+      // set picked hero as input value
+      document.getElementById("inputRadiant"+radiantCounter).value = img.alt;
+
+      radiantCounter += 1;
+    }
+    else if (teamPicking == "dire")
+    {
+      console.log("Picked " + direCounter + " hero for radiant team.");
+      console.log("Radiant slot available : " + (5-direCounter));
+
+      // display picked hero
+      document.getElementById("direHero"+direCounter).src = img.src;
+      document.getElementById("direHero"+direCounter).alt = img.alt;
+      // set picked hero as input value
+      document.getElementById("inputDire"+direCounter).value = img.alt;
+
+      direCounter += 1;
+    }
 
     // make hero picked unclickable and change hero img become empty picture
     img.style.pointerEvents = "none";
     img.src = "../static/images/empty.png";
   }
   else {
-    alert("Tidak dapat memilih hero lebih dari 5")
+    alert("Tidak dapat memilih hero lebih dari 5 untuk setiap tim.")
   }
+}
+
+function setRadiantToPick() {
+  teamPicking = "radiant";
+}
+
+function setDireToPick() {
+  teamPicking = "dire";
 }
